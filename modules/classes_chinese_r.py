76fa112a5be796a2d72bd0de959f1ca6bd783945
MaximOlifer <maximolifer00@gmail.com>
2019-04-06 01:56:29 +0300

    some memory optimization, add method "inverse"

--- modules/classes_chinese_r.py
+++ modules/classes_chinese_r.py
@@ -1,12 +1,44 @@
+def bezout(a, b):
+    '''An implementation of extended Euclidean algorithm.
+    Returns integer x, y and gcd(a, b) for Bezout equation:
+        ax + by = gcd(a, b).
+    '''
+    x, xx, y, yy = 1, 0, 0, 1
+    while b:
+        q = a // b
+        a, b = b, a % b
+        x, xx = xx, x - xx*q
+        y, yy = yy, y - yy*q
+    return (x, y, a)
+
+
+def inverse_el(x, m):
+    return int(bezout(x, m)[0]) % m
+
+
+primes = (97, 101, 103, 107, 109, 113, 127, 131)
+m = 1
+for i in range(len(primes)):
+    m *= primes[i]
+mi = tuple([int(m/primes[i]) for i in range(len(primes))])
+miopp = tuple([inverse_el(mi[i], primes[i]) for i in range(len(primes))])
+
+
 class ChineseRT_Natural:
-    def __init__(self, r1=0, r2=0, r3=0, r4=0, r5=0, r6=0, r7=0, r8=0):
-        self.primes = [97, 101, 103, 107, 109, 113, 127, 131]
-        self.m = 1
-        for i in range(len(self.primes)):
-            self.m *= self.primes[i]
-        self.mi = [int(self.m/self.primes[i]) for i in range(len(self.primes))]
-        self.miopp = [int(self.bezout(self.mi[i], self.primes[i])[0]) % self.primes[i] for i in range(len(self.primes))]
-        self.r = [r1, r2, r3, r4, r5, r6, r7, r8]
+    def __init__(self, arg):
+        self.primes = primes
+        self.m = m
+        self.mi = mi
+        self.miopp = miopp
+        
+        if type(arg) == type(list()):
+            self.r = arg.copy()
+        elif type(arg) == type(int()):
+            self.r = [0 for i in range(len(self.primes))]
+            for i in range(len(self.primes)):
+                self.r[i] = arg % self.primes[i]
+        self.r = tuple(self.r)
+        
         self.x = -1
     
     
@@ -23,40 +55,50 @@ class ChineseRT_Natural:
     
     def __add__(self, other):
         assert type(self) == type(other)
-        res = ChineseRT_Natural()
+        res = [0 for i in range(len(self.primes))]
         for i in range(len(self.primes)):
-            res.r[i] = (self.r[i] + other.r[i]) % self.primes[i]
-        return res
+            res[i] = (self.r[i] + other.r[i]) % self.primes[i]
+        return ChineseRT_Natural(res)
     
     
     def __sub__(self, other):
         assert type(self) == type(other)
-        res = ChineseRT_Natural()
+        res = [0 for i in range(len(self.primes))]
         for i in range(len(self.primes)):
-            res.r[i] = (self.r[i] - other.r[i]) % self.primes[i]
-        return res
+            res[i] = (self.r[i] - other.r[i]) % self.primes[i]
+        return ChineseRT_Natural(res)
     
     
     def __mul__(self, other):
         assert type(self) == type(other)
-        res = ChineseRT_Natural()
+        res = [0 for i in range(len(self.primes))]
+        for i in range(len(self.primes)):
+            res[i] = (self.r[i] * other.r[i]) % self.primes[i]
+        return ChineseRT_Natural(res)
+    
+    
+    def inverse(self):
+        res = [0 for i in range(len(self.primes))]
+        for i in range(len(self.primes)):
+            res[i] = inverse_el(self.r[i], self.primes[i])
+        return ChineseRT_Natural(res) 
+        
+    
+    """
+    def __div__(self, other):
+        assert type(self) == type(other)
+        res = ChineseRT_Natural(0)
         for i in range(len(self.primes)):
             res.r[i] = (self.r[i] * other.r[i]) % self.primes[i]
-        return res     
+        return res
+    """
     
+    """
+    def __mod__(self, other):
+        assert type(self) == type(other)
+        res = ChineseRT_Natural(0)
+        for i in range(len(self.primes)):
+            res.r[i] = (self.r[i] * other.r[i]) % self.primes[i]
+        return res
+    """    
     
-    def bezout(self, a, b):
-        '''An implementation of extended Euclidean algorithm.
-        Returns integer x, y and gcd(a, b) for Bezout equation:
-            ax + by = gcd(a, b).
-        '''
-        x, xx, y, yy = 1, 0, 0, 1
-        while b:
-            q = a // b
-            a, b = b, a % b
-            x, xx = xx, x - xx*q
-            y, yy = yy, y - yy*q
-        return (x, y, a)
-    
-        
-        
