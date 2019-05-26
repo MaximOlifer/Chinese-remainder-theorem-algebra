7bc74fb8a8d4a6e671c1f8e8430fb21d5aeda715
Maxim Olifer <maximolifer00@gmail.com>
2019-05-26 22:07:35 +0300

    Я сделаль сравнение 1 и деление

--- modules/classes_chinese_r.py
+++ modules/classes_chinese_r.py
@@ -16,12 +16,17 @@ def inverse_el(x, m):
     return int(bezout(x, m)[0]) % m
 
 
+# Main constants
 primes = (97, 101, 103, 107, 109, 113, 127, 131)
 m = 1
 for i in range(len(primes)):
     m *= primes[i]
 mi = tuple([m // primes[i] for i in range(len(primes))])
 miopp = tuple([inverse_el(mi[i], primes[i]) for i in range(len(primes))])
+
+# For comparsion 1
+Sq = sum(primes)
+si = [( -inverse_el(mi[i], Sq)) % Sq  for i in range(len(primes))]
 
 
 class ChineseRT_Natural:
@@ -31,6 +36,8 @@ class ChineseRT_Natural:
         self.mi = mi
         self.miopp = miopp
         self.x = -1
+        self.d = -1
+        self._compare_mod = 1
         
         if type(arg) == type(list()) and len(arg) == len(self.primes):
             self.r = arg.copy()
@@ -59,6 +66,10 @@ class ChineseRT_Natural:
                 self.x += self.r[i]*self.mi[i]*self.miopp[i]
             self.x %= self.m
             return self.x
+        
+    
+    def __str__(self):
+        return ("ChineseRT_Natural(" + str(self.value()) + ")")    
         
         
     def __eq__(self, other):
@@ -101,17 +112,6 @@ class ChineseRT_Natural:
         for i in range(len(self.primes)):
             res[i] = inverse_el(self.r[i], self.primes[i])
         return ChineseRT_Natural(res)
-        
-        
-    """
-    def __floordiv__(self, other):
-        assert type(self) == type(other)
-        res = [0 for i in range(len(self.primes))]
-        zero = ChineseRT_Natural(0)
-        for i in range(len(self.primes)):
-            res[i] = (self.r[i] - other.r[i]) % self.primes[i]
-        return ChineseRT_Natural(res)
-    """
     
     
     def __truediv__(self, other):
@@ -123,9 +123,79 @@ class ChineseRT_Natural:
         return ChineseRT_Natural(res)
         
     
-    """
+    # For comparsion 1
+    def _d_function(self):
+        return (sum([si[i]*self.r[i] \
+                     for i in range(len(primes))])) % Sq
+    
+    # Comparsion 1
+    def _compare1(self, other):
+        assert type(self) == type(other)
+        if self.d == -1:
+            self.d = self._d_function()
+        if other.d == -1:
+            other.d = other._d_function()
+            
+        if self.d > other.d:
+            return 2
+        elif self.d < other.d:
+            return 1
+        else:
+            if self.r[0] > other.r[0]:
+                return 2
+            elif self.r[0] < other.r[0]:
+                return 1
+            else:
+                return 0
+       
+    
+    def _compare(self, other):
+        if self._compare_mod == 1:
+            return self._compare1(other)
+            
+            
+    def __lt__(self, other):
+        assert type(self) == type(other)
+        c = self._compare(other)
+        return c == 1
+        
+        
+    def __le__(self, other):
+        assert type(self) == type(other)
+        c = self._compare(other)
+        return c == 1 or c == 0
+    
+    
+    def __gt__(self, other):
+        assert type(self) == type(other)
+        c = self._compare(other)
+        return c == 2
+    
+    
+    def __ge__(self, other):
+        assert type(self) == type(other)
+        c = self._compare(other)
+        return c == 2 or c == 0        
+            
+    
+    def _divide(self, other):
+        assert type(self) == type(other)
+        x, y = self, other
+        
+        result = 0
+        while x >= y:
+            result += 1
+            x -= y
+        
+        return ChineseRT_Natural(result), x
+    
+    
+    def __floordiv__(self, other):
+        assert type(self) == type(other)
+        return self._divide(other)[0]
+        
+        
     def __mod__(self, other):
         assert type(self) == type(other)
-        return self - other*(self // other)
-    """
-    
+        return self._divide(other)[1]
+        
---
+++ modules/debug_chinese_r.py
@@ -0,0 +1,7 @@
+from classes_chinese_r import *
+from random import randrange
+
+n = ChineseRT_Natural(2)
+m = ChineseRT_Natural(10)
+
+print(m % n)
